7458f7af917950f391e16c1c8a9503fb8bd12632
mmehmoodoii <120350575+mmehmoodoii@users.noreply.github.com>
2022-12-13 07:36:23 +0500

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\ASUS\\Desktop\\flutter tools\\Development\\sdks\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\ASUS\\Development\\Projects\\flutter_complete_guide" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\ASUS\\Desktop\\flutter tools\\Development\\sdks\\flutter"
+  "PROJECT_DIR=C:\\Users\\ASUS\\Development\\Projects\\flutter_complete_guide"
+  "FLUTTER_ROOT=C:\\Users\\ASUS\\Desktop\\flutter tools\\Development\\sdks\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\ASUS\\Development\\Projects\\flutter_complete_guide\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\ASUS\\Development\\Projects\\flutter_complete_guide"
+  "FLUTTER_TARGET=C:\\Users\\ASUS\\Development\\Projects\\flutter_complete_guide\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\ASUS\\Development\\Projects\\flutter_complete_guide\\.dart_tool\\package_config.json"
+)
